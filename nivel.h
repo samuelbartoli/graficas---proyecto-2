0e8cf5dbc56e246f61c596de45c03f6883acfbc4
Samuel B'artoli <smbartoli@gmail.com>
2011-11-15 23:00:49 -0430

    structs

---
+++ nivel.h
@@ -0,0 +1,23 @@
+typedef struct {
+    float x;
+    float y;
+    float z;
+}punto;
+
+typedef struct{
+    float shot_v;
+    float trayec_v;
+    punto *points;
+
+    oponentes(int A):points(new float[A]){}
+    ~oponentes(){delete[] points;}
+}oponentes;
+
+typedef struct {
+    int level_id;
+    int game_time;
+    float player_shot_v;
+    float trayec_v;
+    puntos *points;
+    oponentes opo[3];
+}nivel;
